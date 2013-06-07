41b1f33f6fd6ad9e53757dfce86c4a6bedfdec33
zaleski <zaleski@dalembert.upmc.fr>
2013-06-07 17:48:44 +0000

    low porosity case

---
+++ Tests/PenetrableSpheres/plotperm.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#set -x
+
+if [ $# -lt 1 ]; then
+    echo "missing arguments"
+    echo usage $0 nx0 
+    exit
+fi
+
+
+cp perm-$1-all.txt permgp.txt
+gnuplot <<EOF &
+load "perm.gp"
+pause mouse any 
+set term pdf
+set out 'tmp.pdf'
+load "perm.gp"
+ exit
+EOF
