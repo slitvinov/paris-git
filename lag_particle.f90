6736d03b24a067602c34266fbd78da0367efbb20
ling.stanley <ling.stanley@gmail.com>
2013-10-21 09:22:19 +0000

    patch_convert-drop2part_21Oct2013.txt

--- lag_particle.f90
+++ lag_particle.f90
@@ -35,6 +35,8 @@
    use module_tmpvar
    use module_VOF
    use module_2phase
+   use module_flow
+   use module_output_vof
    implicit none
    integer, dimension(:,:,:), allocatable :: tag_id
    integer, dimension(:,:,:), allocatable :: tag_flag
@@ -43,51 +45,47 @@
       ! 2 marked as S node
       ! 3 marked as C node
       ! 4 marked as reference fluid 
-   integer :: max_num_drop = 10000
+   integer,parameter :: max_num_drop = 10000
+   integer,parameter :: maxnum_cell_drop = 500
    logical :: tag_drop_initialized = .false.
    integer :: totalnum_drop
    integer, dimension(:), allocatable :: num_drop
 
-   !   real(8), dimension(:,:), allocatable :: xdrop,udrop
-!      ! (:,:) = (dimension,particle_id)
-!   real(8), dimension(:,:), allocatable :: prop_drop
-!      ! (:,:) = (property-index,particle_id)
-!      ! property index: 1, temperature 2, volume
-!   integer, dimension(:),   allocatable :: droptag
-!      ! (:) = (particle_id)
-   type drop
-      integer :: tag
+   type element 
+      integer :: id 
       real(8) :: xc,yc,zc,uc,vc,wc,vol,tem
+   end type element
+
+   type drop
+      type(element) :: element
+      integer :: num_cell_drop
+      integer :: cell_list(3,maxnum_cell_drop)
    end type drop
    type (drop), dimension(:), allocatable :: drops
-
    
    integer :: max_num_part = 10000
-   logical :: LP_initialized = .false.
-   integer :: num_part 
-   real(8), dimension(:,:), allocatable :: xpart,upart
-      ! (:,:) = (dimension,particle_id)
-   real(8), dimension(:,:), allocatable :: prop_part
-      ! (:,:) = (property-index,particle_id)
-      ! property index: 1, temperature 2, volume
+   logical :: LPP_initialized = .false.
+   integer, dimension(:), allocatable :: num_part
 
+   type particle
+      type(element) :: element
+   end type particle 
+   type (particle), dimension(:), allocatable :: parts
+
+   real(8) :: vol_cut
 contains
 !=================================================================================================
-   subroutine initialize_Lag_part()
-      allocate( xpart    (3,max_num_part) )
-      allocate( upart    (3,max_num_part) )
-      allocate( prop_part(2,max_num_part) )
-   end subroutine initialize_Lag_part
+   subroutine initialize_LPP()
+      allocate( parts(max_num_part) )
+      allocate( num_part(0:nPdomain-1) )
+      num_part = 0
+   end subroutine initialize_LPP
 
    subroutine initialize_tag_drop()
       allocate( num_drop (0:nPdomain-1) )
       num_drop = 0
       allocate( tag_flag(imin:imax,jmin:jmax,kmin:kmax) )
       allocate( tag_id  (imin:imax,jmin:jmax,kmin:kmax) )
-!      allocate( xdrop    (3,max_num_drop) )
-!      allocate( udrop    (3,max_num_drop) )
-!      allocate( prop_drop(2,max_num_drop) )
-!      allocate( droptag  (  max_num_drop) )
       allocate( drops(max_num_drop) )
    end subroutine initialize_tag_drop
 
@@ -103,7 +101,7 @@ contains
     integer :: ns_queue,is_queue,nc_queue
     integer :: ierr,irank
     
-    real :: volscale
+    real :: volscale,cvof_scaled
 
     if (.not. tag_drop_initialized) then 
       call initialize_tag_drop()
@@ -114,7 +112,14 @@ contains
     tag_flag(:,:,:) = 0
     current_id = 1
 
-    drops(:)%vol = 0.d0
+    drops(:)%element%vol = 0.d0
+    drops(:)%element%xc = 0.d0
+    drops(:)%element%yc = 0.d0
+    drops(:)%element%zc = 0.d0
+    drops(:)%element%uc = 0.d0
+    drops(:)%element%vc = 0.d0
+    drops(:)%element%wc = 0.d0
+    drops(:)%num_cell_drop = 0
     volscale = 1.d0/(real(Nx)*real(Ny)*real(Nz))
 
     ns_queue = 0
@@ -152,11 +157,24 @@ contains
             enddo;enddo;enddo ! i0,j0,k0
             tag_flag(isq,jsq,ksq) = 1 !unmark S node and marked as tagged
             ! perform droplet calculation
-            drops(current_id)%tag = current_id
-            drops(current_id)%vol = drops(current_id)%vol & 
-                                    + cvof(isq,jsq,ksq)*volscale
-            drops(current_id)%xc  = drops(current_id)%xc  & 
-                                    + cvof(isq,jsq,ksq)*volscale*x(isq)
+            drops(current_id)%element%id = current_id
+            cvof_scaled = cvof(isq,jsq,ksq)*volscale
+            drops(current_id)%element%vol = drops(current_id)%element%vol & 
+                                          + cvof_scaled
+            drops(current_id)%element%xc  = drops(current_id)%element%xc  & 
+                                          + cvof_scaled*x(isq)
+            drops(current_id)%element%yc  = drops(current_id)%element%yc  & 
+                                          + cvof_scaled*y(jsq)
+            drops(current_id)%element%zc  = drops(current_id)%element%zc  & 
+                                          + cvof_scaled*z(ksq)
+            drops(current_id)%element%uc  = drops(current_id)%element%uc  & 
+                                          + cvof_scaled*u(isq,jsq,ksq)
+            drops(current_id)%element%vc  = drops(current_id)%element%vc  & 
+                                          + cvof_scaled*v(isq,jsq,ksq)
+            drops(current_id)%element%wc  = drops(current_id)%element%wc  & 
+                                          + cvof_scaled*w(isq,jsq,ksq)
+            drops(current_id)%num_cell_drop = drops(current_id)%num_cell_drop + 1
+            drops(current_id)%cell_list(1:3,drops(current_id)%num_cell_drop) = [isq,jsq,ksq]
           end do ! is_queue
           ! mark all C nodes as S nodes
           if ( nc_queue >= 0 ) then 
@@ -169,11 +187,16 @@ contains
          tag_flag(i,j,k) = 4
       end if ! cvof(i,j,k)
     enddo; enddo; enddo
-    drops(:)%xc = drops(:)%xc/(drops(:)%vol+1.0d-60)
+    drops(:)%element%xc = drops(:)%element%xc/(drops(:)%element%vol+1.0d-60)
+    drops(:)%element%yc = drops(:)%element%yc/(drops(:)%element%vol+1.0d-60)
+    drops(:)%element%zc = drops(:)%element%zc/(drops(:)%element%vol+1.0d-60)
+    drops(:)%element%uc = drops(:)%element%uc/(drops(:)%element%vol+1.0d-60)
+    drops(:)%element%vc = drops(:)%element%vc/(drops(:)%element%vol+1.0d-60)
+    drops(:)%element%wc = drops(:)%element%wc/(drops(:)%element%vol+1.0d-60)
 
+    num_drop(rank) = current_id-1
     ! Broadcast num_drop(rank) to all processes
     if ( nPdomain > 1 ) then 
-      num_drop(rank) = current_id-1
       do irank = 0, nPdomain-1
          call MPI_BCAST(num_drop(irank), 1, MPI_INTEGER, &
                         irank, MPI_Comm_Cart, ierr)
@@ -240,44 +263,70 @@ contains
 ! ==============================================
    subroutine output_DP()
       integer :: i,j,k
-      integer :: ib
+      integer :: ib,ipart
       real(8), parameter :: PI = 3.14159265359d0
 
       type(drop), dimension(NumBubble) :: drops_ex
 
+      ! tag droplets and calculate drop properties
       call tag_drop()
       call tag_drop_all()
 
-      OPEN(UNIT=90,FILE=TRIM(out_path)//'/dropvof_before_'//TRIM(int2text(rank,padding))//'.dat')
+      OPEN(UNIT=90,FILE=TRIM(out_path)//'/VOF_before_'//TRIM(int2text(rank,padding))//'.dat')
       write(90,*) 'title= " VOF drops before conversion "'
       write(90,*) 'variables = "x", "y", "z", "c", "tag" '
       write(90,*) 'zone i=,',Nx/nPx+Ng*2, 'j=',Ny/nPy+Ng*2, 'k=',Nz/nPz+Ng*2,'f=point'
       do k = kmin,kmax
          do j=jmin,jmax
             do i=imin,imax 
-               write(90,'(3(I5,1X),(E15.8),(I5))') i,j,k,cvof(i,j,k),tag_id(i,j,k)
+               write(90,'(4(E15.8,1X),(I5))') x(i),y(j),z(k),cvof(i,j,k),tag_id(i,j,k)
             end do ! i
          end do ! j
       end do ! k
       CLOSE(90)
 
-      ! convert droplets to particles
       OPEN(UNIT=91,FILE=TRIM(out_path)//'/dropvol-'//TRIM(int2text(rank,padding))//'.dat')
       OPEN(UNIT=92,FILE=TRIM(out_path)//'/dropvol_ex-'//TRIM(int2text(rank,padding))//'.dat')
       call QSort(drops,NumBubble)
       do ib = 1, NumBubble
-         drops_ex(ib)%xc  = xc(ib)
-         drops_ex(ib)%vol = 4.0d0*rad(ib)**3.d0*PI/3.d0
+         drops_ex(ib)%element%xc  = xc(ib)
+         drops_ex(ib)%element%vol = 4.0d0*rad(ib)**3.d0*PI/3.d0
       end do ! i 
       call QSort(drops_ex,NumBubble)
       do ib = 1, NumBubble 
-         write(91,*) drops   (ib)%xc, drops   (ib)%vol
-         write(92,*) drops_ex(ib)%xc, drops_ex(ib)%vol
+         write(91,*) drops   (ib)%element%xc, drops   (ib)%element%vol
+         write(92,*) drops_ex(ib)%element%xc, drops_ex(ib)%element%vol
       end do ! i 
       CLOSE(91)
       CLOSE(92)
 
+      ! convert droplets to particles
+      vol_cut = 4.0d0*0.04d0**3.d0*PI/3.d0
+      call convertDrop2Part()
+
       ! output droplets & particles
+      OPEN(UNIT=93,FILE=TRIM(out_path)//'/VOF_after_'//TRIM(int2text(rank,padding))//'.dat')
+      write(93,*) 'title= " VOF drops after conversion "'
+      write(93,*) 'variables = "x", "y", "z", "c", "tag" '
+      write(93,*) 'zone i=,',Nx/nPx+Ng*2, 'j=',Ny/nPy+Ng*2, 'k=',Nz/nPz+Ng*2,'f=point'
+      do k = kmin,kmax
+         do j=jmin,jmax
+            do i=imin,imax
+               write(93,'(4(E15.8,1X),(I5))') x(i),y(j),z(k),cvof(i,j,k),tag_id(i,j,k)
+            end do ! i
+         end do ! j
+      end do ! k
+      CLOSE(93)
+
+!      call output_VOF(0,imin,imax,jmin,jmax,kmin,kmax)
+
+      OPEN(UNIT=94,FILE=TRIM(out_path)//'/LPP_after_'//TRIM(int2text(rank,padding))//'.dat')
+      write(94,*) 'title= " Lagrangian particles after conversion "'
+      write(94,*) 'variables = "x", "y", "z"'
+      do ipart = 1,num_part(rank)
+         write(94,*) parts(ipart)%element%xc,parts(ipart)%element%yc,parts(ipart)%element%zc
+      end do ! ipart
+      CLOSE(94)
 
    end subroutine output_DP
 
@@ -326,18 +375,18 @@ contains
 
       ! random pivor (not best performance, but avoids   worst-case)
       call random_number(random)
-      pivot = A(int(random*real(nA-1))+1)%vol
+      pivot = A(int(random*real(nA-1))+1)%element%vol
 
       left = 0
       right = nA + 1
 
       do while (left < right)
          right = right - 1
-         do while (A(right)%vol > pivot)
+         do while (A(right)%element%vol > pivot)
             right = right - 1
          end do
          left = left + 1
-         do while (A(left)%vol < pivot)
+         do while (A(left)%element%vol < pivot)
             left = left + 1
          end do
          if (left < right) then
@@ -358,5 +407,47 @@ contains
 
     end if ! nA
    end subroutine QSort
- 
+
+   subroutine convertDrop2Part()
+
+      integer :: idrop,ilist
+      logical :: convertDropFlag,convertDoneFlag
+
+      if (.not. LPP_initialized) then 
+         call initialize_LPP()
+         LPP_initialized = .true.
+      end if ! LPP_initialized
+
+      convertDropFlag = .false.
+      convertDoneFlag = .false.
+      do idrop = 1,num_drop(rank)
+         if ( drops(idrop)%element%vol < vol_cut ) then 
+            convertDropFlag = .true.
+            ! other criteria can be added here
+         else 
+            convertDropFlag = .false.
+! TEMPORARY ! works only with the vol-cut criteria
+            convertDoneflag = .true. 
+! END TEMPORARY
+         end if 
+
+         if ( convertDoneFlag ) exit
+
+         if ( convertDropFlag ) then
+            ! transfer droplet properties to particle
+            num_part(rank) = num_part(rank) + 1
+            parts(num_part(rank))%element = drops(idrop)%element
+            ! compute average fluid quantities
+
+            ! remove droplet vof structure
+            do ilist = 1,drops(idrop)%num_cell_drop
+               cvof(drops(idrop)%cell_list(1,ilist), &
+                    drops(idrop)%cell_list(2,ilist), &
+                    drops(idrop)%cell_list(3,ilist)) = 0.0
+            end do ! ilist
+         end if !convertDropFlag
+      end do ! idrop
+           
+   end subroutine convertDrop2Part
+
 end module module_Lag_part
