e863eea1bd81aee2be9a2740957ad85c2d1d70ba
Stephane Zaleski <stephane.zaleski@upmc.fr>
2014-01-13 17:56:07 +0000

    removed velocity output to save space temporarily

--- paris.f90
+++ paris.f90
@@ -392,9 +392,10 @@ Program paris
         call calcStats
         call my_timer(2,itimestep,ii)
         if(mod(itimestep,nbackup)==0)call backup_write
+!        if(mod(itimestep,noutuv)==0) then 
         if(mod(itimestep,nout)==0) then 
-           call write_vec_gnuplot(u,v,itimestep)
-           call output(ITIMESTEP/nout,is,ie+1,js,je+1,ks,ke+1)
+           ! call write_vec_gnuplot(u,v,itimestep)
+           ! call output(ITIMESTEP/nout,is,ie+1,js,je+1,ks,ke+1)
            if(DoVOF) call output_VOF(ITIMESTEP/nout,imin,imax,jmin,jmax,kmin,kmax)
            if(DoVOF .and. DoLPP) call output_LPP(ITIMESTEP/nout)
            if(rank==0)then
