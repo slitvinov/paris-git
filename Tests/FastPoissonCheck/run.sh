b1d8c6c55707ab3df51d80bcf4471d6011c5734a
Stephane Zaleski <stephane.zaleski@upmc.fr>
2014-03-29 15:12:48 +0000

    added Poisson solver test

---
+++ Tests/FastPoissonCheck/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#set -x
+
+rm -fR out
+mpirun -np 8 paris > tmpout
+echo `awk ' /Step:/ { cpu = $8 } END { print "cpu = " cpu } ' < tmpout`
+
+RED="\\033[1;31m"
+GREEN="\\033[1;32m"
+NORMAL="\\033[0m"
+
+if [ `grep -c Warning tmpout` -gt 0 ]; then
+    tail -3 tmpout
+    echo -e "$RED" FAIL -e "$NORMAL"
+else
+    echo -e "$GREEN" PASS -e "$NORMAL"
+fi
+
+
+
+
