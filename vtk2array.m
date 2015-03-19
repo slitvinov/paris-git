823b65c37e23988c73b3c7bbcfe3fe28700689fe
Stephane Zaleski <stephane.zaleski@upmc.fr>
2015-03-19 19:05:44 +0000

    new version of matlab tool vtk2array.m
    The tool now works for n number of processes instead of being hard-coded for the default 8 number of processes.

--- vtk2array.m
+++ vtk2array.m
@@ -1,32 +1,67 @@
-function A = vtk2array(folderloc, time)
+function A = vtk2array(folderloc, time, numprocesses)
 %vtk2array
 %   Returns a three dimensional array of the color function at a specific
-%   point in time.  To use, simply input the VTK folder and desired
-%   timestep.  This should work for variables other than the color
+%   point in time.  To use, simply input the VTK folder, desired
+%   timestep, and number of processes.  This should work for variables other than the color
 %   function, but only if the value is described at the center of the grid
 %   cell.  Please feel free to contact me with any comments/concerns.
 %   
-%   Sample Call:
-%   folderloc = '/home/jakedynes/Projects/paris-devel/Tests/VOF/out/VTK/';
-%   time = '00000';
-%   A = vtk2array(folderloc, time)
+%   Note: numprocesses MUST be a perfect cube!
 %
-%   Jake Dynes, August 2014 (dynesj1@montclair.edu)
+%   Sample Call:
+%   folderloc = '/home/jakedynes/paris-stable-8proc-ex/Tests/VOF/out/VTK/';
+%   time = '00000';
+%   numprocesses = 8;
+%   A = vtk2array(folderloc, time, numprocesses)
+%   
+%   Jake Dynes, March 2015 (dynesj1@montclair.edu)
+%
+%   GPL Licence
+%
+%     This file is part of PARIS.
+%
+%     PARIS is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     PARIS is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with PARIS.  If not, see <http://www.gnu.org/licenses/>.
+%
     
-for p=0:7
+for p=0:numprocesses-1
     
     % import data from the current processor
-    intProcessor=strcat('0000',int2str(p));
+    if p<10
+        intProcessor=strcat('0000',int2str(p));
+    elseif p<100 && p>9
+        intProcessor=strcat('000',int2str(p));
+    elseif p<1000 && p>99
+        intProcessor=strcat('00',int2str(p));
+    elseif p<10000 && p>999
+        intProcessor=strcat('0',int2str(p));
+    elseif p<100000 && p>9999
+        intProcessor=strcat('',int2str(p));
+    end
     strFileloc=strcat(folderloc, 'VOF', time, '-', intProcessor, '.vtk');
     matData=importdata(strFileloc);
-    
+    proccube=nthroot(numprocesses,3);
+
     % some setup from first processor
     if p==0
 
         % get grid size (specified in 'input')
-        intXgriddim=(str2double(matData.textdata{5,2})-1)*2;
-        intYgriddim=(str2double(matData.textdata{5,3})-1)*2;
-        intZgriddim=(str2double(matData.textdata{5,4})-1)*2;
+        intXgriddim=(str2double(matData.textdata{5,2})-1)*proccube;
+        intYgriddim=(str2double(matData.textdata{5,3})-1)*proccube;
+        intZgriddim=(str2double(matData.textdata{5,4})-1)*proccube;
+        procXsize=intXgriddim/proccube;
+        procYsize=intYgriddim/proccube;
+        procZsize=intZgriddim/proccube;
 
         % create VOF array (dimensions)
         matVOF=zeros(intZgriddim,intXgriddim,intYgriddim);
@@ -60,185 +95,60 @@ for p=0:7
     end
     
     % sort out ghost values
-    intNumInVOF=(intXgriddim/2)*(intYgriddim/2)*(intZgriddim/2);
+    intNumInVOF=(intXgriddim/proccube)*(intYgriddim/proccube)*(intZgriddim/proccube);
     matCorrectVOF=zeros(intNumInVOF,1);
+    xzero=(p-mod(p,proccube^2))/proccube^2;
+    yzero=floor(mod(p,proccube^2)/proccube);
+    zzero=mod(p,proccube);
     j=1;
+    
     for i=1:intTotalCellswithGhost
         xOK=0;
         yOK=0;
         zOK=0;
         
-        % tedious but necessary formatting for current processor
-        if p==0 % front left bottom processor
-            if and(matPoints(i,1)>0,matPoints(i,1)<xDomain)
-                xOK=1;
-            end
-            if and(matPoints(i,2)>0,matPoints(i,2)<yDomain)
-                yOK=1;
-            end
-            if and(matPoints(i,3)>0,matPoints(i,3)<zDomain)
-                zOK=1;
-            end
-        elseif p==1 % front left top processor   
-            if and(matPoints(i,1)>0,matPoints(i,1)<xDomain)
-                xOK=1;
-            end
-            if and(matPoints(i,2)>0,matPoints(i,2)<yDomain)
-                yOK=1;
-            end
-            if and(matPoints(i,3)>zDomain,matPoints(i,3)<zDomain*2)
-                zOK=1;
-            end
-        elseif p==2 % back left bottom processor
-            if and(matPoints(i,1)>0,matPoints(i,1)<xDomain)
-                xOK=1;
-            end
-            if and(matPoints(i,2)>yDomain,matPoints(i,2)<yDomain*2)
-                yOK=1;
-            end
-            if and(matPoints(i,3)>0,matPoints(i,3)<zDomain)
-                zOK=1;
-            end
-        elseif p==3 % back left top processor
-            if and(matPoints(i,1)>0,matPoints(i,1)<xDomain)
-                xOK=1;
-            end
-            if and(matPoints(i,2)>yDomain,matPoints(i,2)<yDomain*2)
-                yOK=1;
-            end
-            if and(matPoints(i,3)>zDomain,matPoints(i,3)<zDomain*2)
-                zOK=1;
-            end
-        elseif p==4 % front right bottom processor
-            if and(matPoints(i,1)>xDomain,matPoints(i,1)<xDomain*2)
-                xOK=1;
-            end
-            if and(matPoints(i,2)>0,matPoints(i,2)<yDomain)
-                yOK=1;
-            end
-            if and(matPoints(i,3)>0,matPoints(i,3)<zDomain)
-                zOK=1;
-            end
-        elseif p==5 % front right top processor
-            if and(matPoints(i,1)>xDomain,matPoints(i,1)<xDomain*2)
-                xOK=1;
-            end
-            if and(matPoints(i,2)>0,matPoints(i,2)<yDomain)
-                yOK=1;
-            end
-            if and(matPoints(i,3)>zDomain,matPoints(i,3)<zDomain*2)
-                zOK=1;
-            end
-        elseif p==6 % back right bottom processor
-            if and(matPoints(i,1)>xDomain,matPoints(i,1)<xDomain*2)
-                xOK=1;
-            end
-            if and(matPoints(i,2)>yDomain,matPoints(i,2)<yDomain*2)
-                yOK=1;
-            end
-            if and(matPoints(i,3)>0,matPoints(i,3)<zDomain)
-                zOK=1;
-            end
-        elseif p==7 % back right top processor
-            if and(matPoints(i,1)>xDomain,matPoints(i,1)<xDomain*2)
-                xOK=1;
-            end
-            if and(matPoints(i,2)>yDomain,matPoints(i,2)<yDomain*2)
-                yOK=1;
-            end
-            if and(matPoints(i,3)>zDomain,matPoints(i,3)<zDomain*2)
-                zOK=1;
-            end
+        if and(matPoints(i,1)>xzero*xDomain,matPoints(i,1)<(xzero+1)*xDomain)
+            xOK=1;
         end
-        
-        A=[xOK,yOK,zOK];
-        if all(A)
-            matCorrectVOF(j)=matValues(i,1);
-            j=j+1;            
-        end        
-        
+        if and(matPoints(i,2)>yzero*yDomain,matPoints(i,2)<(yzero+1)*yDomain)
+            yOK=1;
+        end
+        %if and(matPoints(i,3)>0,matPoints(i,3)<zDomain)
+        if and(matPoints(i,3)>zzero*zDomain,matPoints(i,3)<(zzero+1)*zDomain)
+            zOK=1;
+        end
+               
+            A=[xOK,yOK,zOK];
+            if all(A)
+                matCorrectVOF(j)=matValues(i,1);
+                j=j+1;            
+            end  
     end
     
-    % now siphon the values from matCorrectVOF into matVOF
-    % again, tedious but necessary formatting
+    %create vof array for current processor
+    matprocVOF=zeros(intZgriddim/proccube, intXgriddim/proccube, intYgriddim/proccube);
     l=1;
-    if p==0
-        for k=intZgriddim:-1:(intZgriddim/2)+1
-            for j=1:(intYgriddim/2)
-                for i=1:(intXgriddim/2)
-                    matVOF(k,i,j)=matCorrectVOF(l);
-                    l=l+1;
-                end
-            end
-        end
-    elseif p==1
-        for k=(intZgriddim/2):-1:1
-            for j=1:(intYgriddim/2)
-                for i=1:(intXgriddim/2)
-                    matVOF(k,i,j)=matCorrectVOF(l);
-                    l=l+1;
-                end
-            end
-        end
-    elseif p==2
-        for k=intZgriddim:-1:(intZgriddim/2)+1
-            for j=(intYgriddim/2)+1:intYgriddim
-                for i=1:(intXgriddim/2)
-                    matVOF(k,i,j)=matCorrectVOF(l);
-                    l=l+1;
-                end
-            end
-        end
-    elseif p==3
-        for k=(intZgriddim/2):-1:1
-            for j=(intYgriddim/2)+1:intYgriddim
-                for i=1:(intXgriddim/2)
-                    matVOF(k,i,j)=matCorrectVOF(l);
-                    l=l+1;
-                end
-            end
-        end
-    elseif p==4
-        for k=intZgriddim:-1:(intZgriddim/2)+1
-            for j=1:(intYgriddim/2)
-                for i=(intXgriddim/2)+1:intXgriddim
-                    matVOF(k,i,j)=matCorrectVOF(l);
-                    l=l+1;
-                end
-            end
-        end
-    elseif p==5
-        for k=(intZgriddim/2):-1:1
-            for j=1:(intYgriddim/2)
-                for i=(intXgriddim/2)+1:intXgriddim
-                    matVOF(k,i,j)=matCorrectVOF(l);
-                    l=l+1;
-                end
-            end
-        end
-    elseif p==6
-        for k=intZgriddim:-1:(intZgriddim/2)+1
-            for j=(intYgriddim/2)+1:intYgriddim
-                for i=(intXgriddim/2)+1:intXgriddim
-                    matVOF(k,i,j)=matCorrectVOF(l);
-                    l=l+1;
-                end
-            end
-        end
-    elseif p==7
-        for k=(intZgriddim/2):-1:1
-            for j=(intYgriddim/2)+1:intYgriddim
-                for i=(intXgriddim/2)+1:intXgriddim
-                    matVOF(k,i,j)=matCorrectVOF(l);
-                    l=l+1;
-                end
+
+    for c=intZgriddim/proccube:-1:1
+        for b=1:intYgriddim/proccube
+            for a=1:intXgriddim/proccube
+                matprocVOF(c,a,b)=matCorrectVOF(l);
+                l=l+1;
             end
         end
     end
     
+    % stick the processor VOF array in the right spot in the overall VOF
+    % array
+    zzero=proccube-mod(p,proccube);
+    yzero=floor(mod(p,proccube^2)/proccube);
+    xzero=floor(p/proccube^2);
+    
+    matVOF(procZsize*(zzero-1)+1:procZsize*zzero,...
+        (xzero*procXsize)+1:(xzero+1)*procXsize,...
+        (yzero*procYsize)+1:(yzero+1)*procYsize)=matprocVOF;
 end
 
 A=matVOF;
 
 end
-
