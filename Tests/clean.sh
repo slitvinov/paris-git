652e56c54b4754860d05eb8c2df3f1f023731e52
Stephane Zaleski <stephane.zaleski@upmc.fr>
2016-06-05 10:53:42 +0000

    minor change. Cleans files more thoroughly in make clean.
    Tests/clean.sh now also cleans the multi00nnn.root files.

--- Tests/clean.sh
+++ Tests/clean.sh
@@ -8,7 +8,7 @@ for dir in `ls`; do
 	cd $dir
 	echo "Cleaning directory " `pwd`
 #	echo "rm -fR *.o *.mod paris stats *~ track out* errftc tmp* *.tmp fort.* *.visit core.*"
-	rm -fR *.o *.mod paris stats *~ track out* errftc tmp* *.tmp fort.* *.visit core.* statsbub out/*.txt testinput-* *.log aggregate_timer_stats
+	rm -fR *.o *.mod paris stats *~ track out* errftc tmp* *.tmp fort.* *.visit core.* statsbub out/*.txt testinput-* *.log aggregate_timer_stats multi*.root
 	cd ..
     fi
 done
