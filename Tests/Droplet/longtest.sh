4f0e69abf57ea359890761b1b0ca657e4fc8a2c8
Stephane Zaleski <stephane.zaleski@upmc.fr>
2016-06-20 17:45:34 +0000

    Fixes the Droplet long test

--- Tests/Droplet/longtest.sh
+++ Tests/Droplet/longtest.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #set -x
 
-momconstrue=T
-nfilter=0
+momconstrue=F
+nfilter=1
+plotlabel=NonMomCons
+
+
 sed s/MOMCONSTEMP/$momconstrue/g inputvof.template | sed s/NFILTERTEMP/$nfilter/g > inputvof 
+sed s/nonMOMCONS/$plotlabel/g plot.gp.template > plot.gp
+
+
 
 rm -fR out input
 ln -s inputlong input
@@ -12,9 +18,11 @@ echo `awk ' /Step:/ { cpu = $8 } END { print "cpu = " cpu } ' < tmpout`
 rm -f input
 
 precision=1e-2
-pariscompare out/droplet-test-vel.txt reference-MomCons.txt $precision 1 0
+pariscompare out/droplet-test-vel.txt reference-$plotlabel.txt $precision 1 0
 
-gnuplot < plotlong.gp
+gnuplot < plot.gp
+
+mv droplet.png ../Testreport
 GREEN="\\033[1;32m"
 NORMAL="\\033[0m"
 
