f10a289ed06d3087f1db9f3babf6e4180b12dfd2
Stephane Zaleski <stephane.zaleski@upmc.fr>
2016-06-20 02:00:03 +0000

    first version of html test report system

--- Tests/Droplet/longtest.sh
+++ Tests/Droplet/longtest.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 #set -x
 
-
-momconstrue=F
-plotlabel=NonMomCons
-nfilter=1
-
-if [ $# -gt 0 ]; then
-    if [ $1 == MomCons ]; then
-	echo "using MomCons"
-	momconstrue=T
-	plotlabel=MomCons
-	nfilter=0
-    fi
-fi
-
-
+momconstrue=T
+nfilter=0
 sed s/MOMCONSTEMP/$momconstrue/g inputvof.template | sed s/NFILTERTEMP/$nfilter/g > inputvof 
-sed s/nonMOMCONS/$plotlabel/g plot.gp.template > plot.gp
-
-
 
 rm -fR out input
 ln -s inputlong input
@@ -27,16 +11,10 @@ mpirun -np 8 paris > tmpout
 echo `awk ' /Step:/ { cpu = $8 } END { print "cpu = " cpu } ' < tmpout`
 rm -f input
 
-gnuplot <<EOF
-set xlabel "time"
-set ylabel "w(0,0,R)"
-set term png
-set out "tmp.png"
-EOF
-
 precision=1e-2
-pariscompare out/droplet-test-vel.txt reference-long-$plotlabel.txt $precision 1 0
+pariscompare out/droplet-test-vel.txt reference-MomCons.txt $precision 1 0
 
+gnuplot < plotlong.gp
 GREEN="\\033[1;32m"
 NORMAL="\\033[0m"
 
